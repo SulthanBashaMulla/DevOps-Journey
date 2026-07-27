323673fc737d3ceea22ba0ca0cf474927a914a65
SulthanBashaMulla <mullasulthanbasha@gmail.com>
2026-07-27 18:18:37 +0000

    second day i practised about the proper method to write a shell script

---
+++ Linux/Script/Day-02/proper-script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+
+
+###################################
+
+#Author: Sulthan
+
+#Date: 27-07-2026
+
+
+#usage: this script tells the proper usage of the method to write a proper script
+
+echo "open this script via vim cmd or in your browser to know the proper way to write the script"
+
+#steps to follow up
+
+
+echo "1. write the shebang line #!/bin/bash "
+echo "2. write the meta properties to the script like Author, Date, Time, Purpose of the script"
+echo "3. use the cmds in a proper sequence in the way you want them to execute"
+echo "4. (optional) you can use the DEBUG Mode use: set -x"
+echo "5. use comments between each cmd to properly mentin the each meaning of the command"
+echo "6. save the file"
+echo "7. change the file permission so that you can execute the script in your machine using chmod  and number sequence ex:- 777"
+echo "8. execute the file using the cmd ./<filename> or using sh <filename>"
+echo "Now, you are a good script writer. Good luck Have fun through scripting"
